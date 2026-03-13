1df498c9c85e7c16f1f9fefdf66a1350b2f0f8f2
SadrunHafizTeknikInformatika <12550112698@students.uin-suska.ac.id>
2026-03-13 15:27:26 +0700

    Update book.py

--- book.py
+++ book.py
@@ -2,7 +2,16 @@ from datetime import datetime
 
 class Book:
     def __init__(self, id, title, author):
-        self.id = id
-        self.title = title
+        self.__id = idx
+        self.__title = title
         self.author = author
         self.last_updated = datetime.now()
+
+    def get_id(self):
+        return self.__id
+
+    def get_title(self):
+        return self.__title
+
+    def self_title(self, new_title):
+        self.__title = new_title
